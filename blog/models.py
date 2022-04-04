6c17afe0bbe094053acc39b18071347b62603dd8
lahodiukoleksandra <81188068+lahodiukoleksandra@users.noreply.github.com>
2022-04-04 09:22:18 +0200

    Change models.py

--- blog/models.py
+++ blog/models.py
@@ -4,5 +4,12 @@ from django.db import models
 from django.db.models import Model
 
 
-class Posts(Model):
+class Posts(models.Model):
+    author = models.TextField(max_length=200)
+    created_at = models.DateTimeField(auto_now_add=True, db_index=True)
+    modified_at = models.DateTimeField(auto_now=True)
+    published_at = models.DateTimeField(blank=True, null=True, db_index=True)
+    title = models.TextField(max_length=100)
     slug = models.SlugField(unique=True)
+    summary = models.TextField(max_length=500)
+    content = models.TextField()
